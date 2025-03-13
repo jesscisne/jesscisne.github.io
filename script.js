503ab7e40ed68e79512246e9053829562e032581
jesscisne <jessicacisnee@gmail.com>
2025-03-13 12:37:56 -0300

    Criando páginas para os menus

---
+++ script.js
@@ -0,0 +1,19 @@
+var sobralTurmas = document.getElementById("sobralTurmas") 
+sobralTurmas.addEventListener("click",function(){
+    window.location.href = "turmas.html"
+}) 
+
+var remotaTurmas = document.getElementById("remotaTurmas")
+remotaTurmas.addEventListener("click",function(){
+    window.location.href = "turmas.html"
+})
+
+var convida = document.getElementById("convida")
+convida.addEventListener("click",function(){
+    window.location.href = "acoes.html"
+})
+
+var noite = document.getElementById("noite")
+noite.addEventListener("click",function(){
+    window.location.href = "acoes.html"
+})
